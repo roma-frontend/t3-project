727c888c1b7a50a43c5b3d3e5450a5e953112456
Роман <125897843+roma-frontend@users.noreply.github.com>
2024-04-18 15:14:41 +0400

    database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
-export default function HomePage() {
+import { db } from "~/server/db";
+
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+
+  console.log(posts);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello World
+    <main className="">
+      {posts.map((post) => (
+        <div key={post.id}>{post.name}</div>
+      ))}
     </main>
   );
 }
